f0228795cdc594cc98f17f360493a33f6db6e2fb
steve <steve@Steves-MBP.fios-router.home>
2019-04-01 14:54:02 -0400

    smf/task: update login config for academy

--- jsLogger/main.js
+++ jsLogger/main.js
@@ -1,13 +1,5 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
-var logger_1 = require("./logger");
-function main() {
-    var L = new logger_1["default"]();
-    L.Log("hello");
-    L.SetEncoding('json');
-    L.SetLogStream('warn');
-    L.Log("world");
-    L.SetLogFile('../test.txt', 'info');
-    L.Log("hello");
-}
+var logger_1 = require('./logger');
+function main() {}
 main();
